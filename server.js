306bccb6d9a3c510e402ee2f2a0c78b4fedc9cec
Matt Higbee <mhigbee@hotmail.com>
2017-08-30 07:51:18 -0600

    saving progress

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
+const Bear = require('./models.js');
 
 const STATUS_USER_ERROR = 422;
 const STATUS_SERVER_ERROR = 500;
@@ -9,17 +10,41 @@ const server = express();
 // allow server to parse JSON bodies from POST/PUT/DELETE requests
 server.use(bodyParser.json());
 
-
-
-
-
-
 // TODO: write your server code here
+server.post('/bears', (req, res) => {
+  //ES 5 var species = req.body.species;
+  //ES 5 var latinName = req.body.latinName;
+  //ES 6 destructuring 
+  const { species, latinName } = req.body;
+  if (!species || !latinName) {
+    res.status(422);
+    res.json({ error: 'Missing species and/or Latin Name' });
+    return;
+  }
+  const bear = new Bear({ species, latinName });
+  bear.save((err) => {
+    if (err) throw err;
+    res.json(bear);
+  });
+});
 
+server.get('/bears', (req, res) => {
+  Bear.find({}, (err, data) => {
+    if (err) throw err;
+    res.json(data);
+  });
+});
 
-
-
-
+server.get('/bears/:id', (req, res) => {
+  const { id } = req.params;
+  Bear.findById(id, (err, bear) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    res.json(bear);
+  });
+});
 
 mongoose.Promise = global.Promise;
 const connect = mongoose.connect(
